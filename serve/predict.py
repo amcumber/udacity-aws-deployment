b19da723d5be1798b84a9093eaaa3af0c8e6e079
Aaron McUmber, Ph.D <aaron.mcumber@gmail.com>
2021-04-04 22:16:24 -0700

    First Submission

--- serve/predict.py
+++ serve/predict.py
@@ -70,8 +70,10 @@ def predict_fn(input_data, model):
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
 
-    data_X = None
-    data_len = None
+#     data_X = None
+#     data_len = None
+    data_sen = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, data_sen)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -87,6 +89,14 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = None
+#     result = None
+    result_raw = model(data)
+#     print('DEBUG:', result_raw)
+    result_raw = result_raw.detach()
+    result_raw = result_raw.cpu()
+    result_raw = result_raw.numpy()
+    result_int = np.round(result_raw)
+#     print('DEBUG:', result_int)
+    result = result_int.astype(int)
 
     return result
--- train/train.py
+++ train/train.py
@@ -25,7 +25,8 @@ def model_fn(model_dir):
 
     # Determine the device and construct the model.
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = LSTMClassifier(model_info['embedding_dim'], model_info['hidden_dim'], model_info['vocab_size'])
+    model = LSTMClassifier(model_info['embedding_dim'], model_info['hidden_dim'], 
+                           model_info['vocab_size'])
 
     # Load the stored model parameters.
     model_path = os.path.join(model_dir, 'model.pth')
@@ -56,20 +57,26 @@ def _get_train_data_loader(batch_size, training_dir):
 
 
 def train(model, train_loader, epochs, optimizer, loss_fn, device):
-    """
-    This is the training method that is called by the PyTorch training script. The parameters
-    passed are as follows:
-    model        - The PyTorch model that we wish to train.
-    train_loader - The PyTorch DataLoader that should be used during training.
-    epochs       - The total number of epochs to train for.
-    optimizer    - The optimizer to use during training.
-    loss_fn      - The loss function used for training.
-    device       - Where the model and data should be loaded (gpu or cpu).
-    """
-    
-    # TODO: Paste the train() method developed in the notebook here.
-
-    pass
+    for epoch in range(1, epochs + 1):
+        model.train()
+        total_loss = 0
+        for batch in train_loader:         
+            batch_X, batch_y = batch
+            
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
+            
+            # TODO: Complete this train method to train the model provided.
+            model.zero_grad()
+            batch_out = model(batch_X)
+            loss = loss_fn(batch_out, batch_y)
+            loss.backward()
+            
+            optimizer.step()
+            
+            
+            total_loss += loss.data.item()
+        print("Epoch: {}, BCELoss: {}".format(epoch, total_loss / len(train_loader)))
 
 
 if __name__ == '__main__':
